27cefdba5c06627560585800a89dd8fc170619b5
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2023-09-14 23:31:38 +0900

    Add /public/index.html

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>第68回2023年芝生祭総務課サイトへようこそ!</title>
+</head>
+<body>
+    <h1>第68回2023年芝生祭総務課サイトへようこそ!</h1>
+    <p>公式サイトは<a href="https://www.fzk.shibaura-it.ac.jp/shibafu/2023/" target="_blank" rel="norefferrer">こちら</a></p>
+    <p>企画投票をされる方は、お手元にパンフレットをご準備の上、<a href="/vote/index.php">こちらへ</a></p>
+</body>
+</html>
