e42b741e5dc3690df075ab0a3ee17b119c46d32f
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2023-09-16 00:34:21 +0900

    Add error.php

---
+++ public/vote/error.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>投票エラー</title>
+    <meta http-equiv="refresh" content="5;URL=https://shibafufes68th.main.jp/vote/index.php">
+</head>
+<body>
+    <h1>投票エラー</h1>
+    <p>投票中にエラーが発生しました。<br>
+    <?php
+        switch($_GET['code']){
+        case "invalid_uid":
+            echo "不正なUID(英字)";
+        }
+    ?>
+    </p>
+    <p>5秒後に投票サイトトップへ移動します。</p>
+</body>
+</html>
