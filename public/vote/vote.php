ef3b18a5ed6ea46afe430258c349024bdd5ee10d
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2023-09-18 14:34:47 +0900

    Update vote.php

--- public/vote/vote.php
+++ public/vote/vote.php
@@ -6,5 +6,16 @@ session_start();
 $db = new DBControlClass();
 
 $result = $db->update(intval($_SESSION['id']), intval($_SESSION['voted_times'])+1, intval($_POST['best_exhibition']), intval($_POST['best_poster']), $_POST['email'], $_POST['impression']);
-echo $result
+
+session_destroy();
 ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>第68回2023芝生祭投票ページ</title>
+    <meta http-equiv="refresh" content="3;URL=/index.php">
+</head>
+<body>
+    <p><?php echo $result; ?></p>
+</body>
+</html>
