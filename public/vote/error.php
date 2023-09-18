333bb2820d0995d029701fd9a7f4665e0b160c29
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2023-09-18 21:04:15 +0900

    Add edit.phpのcss , error.phpのcss

--- public/vote/error.php
+++ public/vote/error.php
@@ -5,18 +5,23 @@ session_destroy();
 <head>
     <meta charset="UTF-8">
     <title>投票エラー</title>
+    <link href="./img/favicon.ico" rel="icon" />
+    <link href="./css/common.css" rel="stylesheet" />
+    <link href="./css/voteform.css" rel="stylesheet" />
     <meta http-equiv="refresh" content="5;URL=/vote/index.php">
 </head>
 <body>
+<main>
     <h1>投票エラー</h1>
     <p>投票中にエラーが発生しました。<br>
     <?php
         switch($_GET['code']){
         case "invalid_uid":
-            echo "不正なUID(英字)";
+            echo "不正なUID(英数字)";
         }
     ?>
     </p>
     <p>5秒後に投票サイトトップへ移動します。</p>
+</main>
 </body>
 </html>
