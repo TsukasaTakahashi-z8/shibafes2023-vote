65fe2a315ef93ead385f6e88bbfc3410969dc43c
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2023-09-18 14:18:15 +0900

    Add best_poster options in voteform.php

--- public/vote/voteform.php
+++ public/vote/voteform.php
@@ -21,25 +21,25 @@ $uid_check->redirect();
                 $f = fopen("./config/exhibition.csv", 'r');
                 while ($line = fgetcsv($f)){
                     if ($line[0]=="企画id"){
-                        echo "            <option selected disable hidden>{$line[3]}（{$line[2]}）</option>";
+                        echo "            <option class=\"select_exhibition\"selected disable hidden>{$line[3]}（{$line[2]}）</option>";
                         continue;
                     }
-                    echo "            <option value=\"{$line[0]}\">{$line[3]}（{$line[2]}）</option>";
+                    echo "            <option class=\"select_exhibition\" value=\"{$line[0]}\">{$line[3]}（{$line[2]}）</option>";
                 }
+                fclose($f);
             ?>
         </select>
-        <select name="best_poster" required>
             <?php
                 $f = fopen("./config/exhibition.csv", 'r');
                 while ($line = fgetcsv($f)){
-                    if ($line[0]=="企画id"){
-                        echo "            <option selected disable hidden>{$line[3]}（{$line[2]}）</option>";
-                        continue;
+                    $imgs = glob("./img/{$line[0]}.*");
+                    if ($line[0]!="企画id" && isset($imgs[0])){
+                        echo "        <input class=\"radio_poster\" id=\"poster_{$line[0]}\" type=\"radio\" name=\"best_poster\" value=\"{$line[0]}\">";
+                        echo "        <label for=\"poster_{$line[0]}\"><img class=\"poster\" src=\"{$imgs[0]}\"></label>";
                     }
-                    echo "            <option value=\"{$line[0]}\">{$line[3]}（{$line[2]}）</option>";
                 }
+                fclose($f);
             ?>
-        </select>
         <h2>ここからは、任意です。</h2>
         <p>頂いたご意見は、今後の芝生祭運営に役立たせていきます。<br>emailを記入していただいた場合は、数週間以内に実行委員会からお返事をさせていただきます。</p>
         <input type="email" name="email">
